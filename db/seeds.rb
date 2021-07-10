6b7e1b811763495c1052dd4ab99485699beb9b71
Felipe Cuevas <felipe@softwareallies.com>
2021-07-09 21:36:35 -0500

    db/seeds.rb change builder to use contry code ISO 3166- Alpha2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,18 @@ require 'faker'
 #   Character.create(name: 'Luke', movie: movies.first)
 
 (1..6).each_with_index do |a, i|
-  r = RealEstate.create( name: Faker::Name.name, real_state_type: "house", street: Faker::Address.street_name, external_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country,  rooms: rand(1..10), bathrooms: rand(1..3) )
+  r = RealEstate.create( name: Faker::Name.name, real_state_type: "house", street: Faker::Address.street_name, external_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country_code,  rooms: rand(1..10), bathrooms: rand(1..3) )
 end
 
 (1..6).each_with_index do |a, i|
-  r = RealEstate.create( name: Faker::Name.name, real_state_type: "department", street: Faker::Address.street_name, external_number: Faker::Address.building_number, internal_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country,  rooms: rand(1..10), bathrooms: rand(1..3) )
+  r = RealEstate.create( name: Faker::Name.name, real_state_type: "department", street: Faker::Address.street_name, external_number: Faker::Address.building_number, internal_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country_code,  rooms: rand(1..10), bathrooms: rand(1..3) )
 end
 
 (1..6).each_with_index do |a, i|
-  r = RealEstate.create( name: Faker::Name.name, real_state_type: "land", street: Faker::Address.street_name, external_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country,  rooms: rand(1..10), bathrooms: rand(0..3) )
+  r = RealEstate.create( name: Faker::Name.name, real_state_type: "land", street: Faker::Address.street_name, external_number: Faker::Address.building_number, neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country_code,  rooms: rand(1..10), bathrooms: rand(0..3) )
   p "r #{a} #{r.errors}"
 end
 
 (1..6).each_with_index do |a, i|
-  r = RealEstate.create( name: Faker::Name.name, real_state_type: "commercial_ground", street: Faker::Address.street_name, external_number: Faker::Address.building_number, internal_number: Faker::Address.building_number,neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country,  rooms: rand(1..10), bathrooms: rand(0..3) )
+  r = RealEstate.create( name: Faker::Name.name, real_state_type: "commercial_ground", street: Faker::Address.street_name, external_number: Faker::Address.building_number, internal_number: Faker::Address.building_number,neighborhood: "neighborhood #{a}", city: Faker::Address.city, country:  Faker::Address.country_code,  rooms: rand(1..10), bathrooms: rand(0..3) )
 end
